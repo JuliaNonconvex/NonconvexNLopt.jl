e13aa7eb4fff6522b7bc1ec9bf1aedca89308ea8
Mohamed Tarek <mohamed82008@gmail.com>
2021-08-31 19:02:09 +1000

    move code from Nonconvex

--- src/NonconvexNLopt.jl
+++ src/NonconvexNLopt.jl
@@ -1,5 +1,14 @@
 module NonconvexNLopt
 
-# Write your package code here.
+export NLoptAlg, NLoptOptions
+
+using Reexport, Parameters, SparseArrays, Zygote
+@reexport using NonconvexCore
+using NonconvexCore: @params, VecModel, AbstractResult
+using NonconvexCore: AbstractOptimizer, CountingFunction
+import NonconvexCore: optimize!
+using NLopt
+
+include("nlopt.jl")
 
 end
